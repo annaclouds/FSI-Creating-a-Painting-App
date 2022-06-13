f492fb52cd3c9d53efcff948aface872fffefbab
annaclouds <napha@ymail.com>
2022-06-13 02:50:34 -0700

    added comments and psuedocode for implementing the color pallete.

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-// select the painting class.
+// 1. Select the painting class.
+// 5. Use a variable to represent the selected color.
+let selectedColor = 'blue';                         // use a variable instead of hard-coding the pixel's backgroundColor to blue.
 const painting = document.querySelector('.painting');
 
-// track when the painting is clicked.
-    // painting.addEventListener('click', function() {
-    //     console.log('testing, testing, 123');
-    // });
+// 2. Track when the painting is clicked.
+// 3. Reference the clicked pixel.
+painting.addEventListener('click', function(e) {    // accepting e in our event listener to change the color of our pixel, which is our target of the click event.
+    // console.log('testing, testing, 123');
+    console.log(e.target);                          // referencing the target property of the e object.
 
-// reference the clicked pixel.
-painting.addEventListener('click', function(e) { // element that was clicked will be the target of the click event, so we access e in our function.
-//     console.log('testing, testing, 123');
-// });
 
-// referencing the target property of the e object.
-    // console.log(e.target);
-    // });
-
-// change the backgroundColor of the clicked pixel.
-    e.target.style.backgroundColor = 'blue'
+// 4. Change the backgroundColor of the clicked pixel.
+// e.target references a DOM element, just like the ones we create or query.
+    e.target.style.backgroundColor = selectedColor;
 });
+
+// 7. Implement the color pallete.
+
+/* psuedocode:
+1. user clicks on red square.
+2. the color is changed to red. --> event listener with e target.
+3. user clicks on painting.
+4. red square appears.
+
+1. user clicks on yellow square.
+2. the color is changed to yellow. --> event listener with e target.
+3. user clicks on painting.
+4. yellow square appears.
+
+1. user clicks on green square.
+2. the color is changed to green. --> event listener with e target.
+3. user clicks on painting.
+4. green square appears.
